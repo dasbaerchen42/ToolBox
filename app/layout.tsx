e20c69efb698361127dae81ec15e5d6a206c0add
Das Baerchen <68527574+dasbaerchen42@users.noreply.github.com>
2026-04-07 13:36:39 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import SiteNavigation from "@/components/site-navigation";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "My Writing App",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className="font-mono tracking-[0.04em] leading-7 antialiased">
         <SiteNavigation />
         {children}
+        <Analytics />
       </body>
     </html>
   );
